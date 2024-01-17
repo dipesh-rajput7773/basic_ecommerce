f11b015b82d5479c3bb3a6bac29fdbcf8f545bcc
dipesh-rajput7773 <60784960+dipesh-rajput7773@users.noreply.github.com>
2024-01-17 22:29:52 +0530

    Update Singdw

--- src/Layouts/Products/Product/SingleProduct.js
+++ src/Layouts/Products/Product/SingleProduct.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+ import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Helmet } from 'react-helmet';
@@ -18,7 +18,29 @@ function SingleProduct() {
     }, [id]);
 
     const handleCart = (product, redirect) => {
-        // ... (unchanged)
+        console.log(singleProduct);
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        const isProductExist = cart.find(item => item.id === singleProduct.id);
+
+        if (isProductExist) {
+            const updatedCart = cart.map(item => {
+                if (item.id === singleProduct.id) {
+                    return {
+                        ...item, quantity: item.quantity + 1
+                    };
+                }
+                return item;
+            });
+            localStorage.setItem('cart', JSON.stringify(updatedCart));
+        } else {
+            localStorage.setItem('cart', JSON.stringify([...cart, { ...singleProduct, quantity: 1 }]));
+        }
+
+        alert('Product added to cart');
+        
+        if (redirect) {
+            navigate('/cart');
+        }
     };
 
     if (!singleProduct) {
@@ -27,48 +49,58 @@ function SingleProduct() {
 
     const productUrl = window.location.href;
 
+ 
+
     return (
         <div className='single-product-page'>
-            <>
-                <Helmet>
-                    <title>{singleProduct.title}</title>
-                    <meta property="og:title" content={singleProduct.title} />
-                    <meta property="og:description" content={singleProduct.description} />
-                    <meta property="og:image" content={singleProduct.image} />
-                    <meta property="og:url" content={productUrl} />
-                    <meta property="og:type" content="product" />
-                    <meta property="product:price:amount" content={singleProduct.price} />
-                    {/* Add more meta tags as needed */}
-                </Helmet>
+            <Helmet>
+                <title>{singleProduct.title}</title>
+                <meta property="og:title" content={singleProduct.title} />
+                <meta property="og:description" content={singleProduct.description} />
+                <meta property="og:image" content={singleProduct.image} />
+                <meta property="og:url" content={productUrl} />
+                <meta property="og:type" content="product" />
+                <meta property="product:price:amount" content={singleProduct.price} />
+                {/* Add more meta tags as needed */}
+            </Helmet>
 
-                <div className='row'>
-                    <div className='col-md-6'>
-                        <div className='single-product-image'>
-                            <img src={singleProduct.image} alt={singleProduct.title} />
-                        </div>
-                    </div>
-
-                    <div className='col-md-6'>
-                        <div className='single-product-details'>
-                            {/* ... (unchanged) */}
-                        </div>
+            <div className='row'>
+                <div className='col-md-6'>
+                    <div className='single-product-image'>
+                        <img src={singleProduct.image} alt={singleProduct.title} />
                     </div>
                 </div>
 
-                <div>
-                    {/* Facebook Share Button */}
-                    <button onClick={() => handleFacebookShare(productUrl)}>
-                        Share on Facebook
-                    </button>
+                <div className='col-md-6'>
+                    <div className='single-product-details'>
+                        <div className='single-product-name'>{singleProduct.title}</div>
+                        <div className='single-product-desc'>{singleProduct.description}</div>
+                        <div className='single-product-price'>${singleProduct.price}</div>
+                        <div className='single-product-desc'>{singleProduct.category}</div>
+                        <div className='cart'>
+                            <button className='button' onClick={() => handleCart(singleProduct)}>
+                                Add to Cart
+                            </button>
+                            <button className='button' onClick={() => handleCart(singleProduct, true)}>
+                                Buy it now
+                            </button>
+                        </div>
+
+                        <div>
+                            {/* Facebook Share Button */}
+                            <a
+                                href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(productUrl)}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Share on Facebook
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </>
+            </div>
         </div>
     );
 }
 
 export default SingleProduct;
-
-function handleFacebookShare(url) {
-    // Open Facebook share dialog
-    window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`, '_blank');
-}
